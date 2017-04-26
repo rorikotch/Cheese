3fbc341f2cd6e8b3922b1862c2354aaf34b2a319
Rori Kotch <r.kotch1@umiami.edu>
2017-04-26 19:00:57 -0400

    Changed stuff

--- script.js
+++ script.js
@@ -138,37 +138,37 @@ Highcharts.chart("container", {
     });
 
 
-// Highcharts.chart("containerfour", {
-//
-//         title: {
-//             text: "U.S. Milk Production 2010 to 2017"
-//         },
-//
-//         subtitle: {
-//             text: "This data was sourced from the U.S.D.A. Department of Agriculture"
-//         },
-//
-//         yAxis: {
-//             title: {
-//                 text: "Millions of Pounds"
-//             }
-//         },
-//
-//         xAxis: {
-//           categories: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
-//             title: {
-//                 text: "Years"
-//             }
-//         },
-//
-//         plotOptions: {
-//             series: {
-//                 pointStart: 2010
-//             }
-//         },
-//
-//         series: [{
-//             name: "Production",
-//             data: [16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
-//         }]
-//       })
+Highcharts.chart("containerfour", {
+
+        title: {
+            text: "U.S. Milk Production 2010 to 2017"
+        },
+
+        subtitle: {
+            text: "This data was sourced from the U.S.D.A. Department of Agriculture"
+        },
+
+        yAxis: {
+            title: {
+                text: "Millions of Pounds"
+            }
+        },
+
+        xAxis: {
+          categories: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+            title: {
+                text: "Years"
+            }
+        },
+
+        plotOptions: {
+            series: {
+                pointStart: 2010
+            }
+        },
+
+        series: [{
+            name: "Production",
+            data: [16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
+        }]
+      })
