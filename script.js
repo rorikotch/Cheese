3cd96d15a6db5e6d0db8d552aa96f9e6ed9fd0b3
Rori Kotch <r.kotch1@umiami.edu>
2017-04-26 15:28:40 -0400

    Altered data

--- script.js
+++ script.js
@@ -79,6 +79,13 @@ Highcharts.chart("container", {
             text: 'Source: U.S.D.A. ERS'
         },
 
+        xAxis: {
+          categories: [1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+            title: {
+                text: "Years"
+            }
+        },
+
         yAxis: {
             title: {
                 text: 'Number of Dairy Cows'
