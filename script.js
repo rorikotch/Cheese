bca23e224b0b5d086d0ff47621403be019eb9d66
Rori Kotch <r.kotch1@umiami.edu>
2017-04-26 18:43:52 -0400

    Changed things

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 Highcharts.chart("container", {
 
     title: {
@@ -32,7 +31,7 @@ Highcharts.chart("container", {
         name: "Production",
         data: [492980000, 501954000, 520389000, 517261000, 538029000, 561722000, 589951000, 598166000, 617245000, 636971000, 693003000, 693797000, 713939000, 709599000, 738086000, 754667000, 782370000, 828272000, 819441000, 823902000, 842519000, 883505000, 911773000, 931691000, 962429000, 994542000, 1003278000, 1044983000]
     }]
-  })
+  });
 
   Highcharts.chart("containertwo", {
 
@@ -139,37 +138,37 @@ Highcharts.chart("container", {
     });
 
 
-    Highcharts.chart("Chart", {
-
-        title: {
-            text: "U.S. Milk Production 2010 to 2017"
-        },
-
-        subtitle: {
-            text: "This data was sourced from the U.S.D.A. Department of Agriculture"
-        },
-
-        yAxis: {
-            title: {
-                text: "Millions of Pounds"
-            }
-        },
-
-        xAxis: {
-          categories: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
-            title: {
-                text: "Years"
-            }
-        },
-
-        plotOptions: {
-            series: {
-                pointStart: 2010
-            }
-        },
-
-        series: [{
-            name: "Production",
-            data: [16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
-        }]
-      })
+// Highcharts.chart("containerfour", {
+//
+//         title: {
+//             text: "U.S. Milk Production 2010 to 2017"
+//         },
+//
+//         subtitle: {
+//             text: "This data was sourced from the U.S.D.A. Department of Agriculture"
+//         },
+//
+//         yAxis: {
+//             title: {
+//                 text: "Millions of Pounds"
+//             }
+//         },
+//
+//         xAxis: {
+//           categories: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+//             title: {
+//                 text: "Years"
+//             }
+//         },
+//
+//         plotOptions: {
+//             series: {
+//                 pointStart: 2010
+//             }
+//         },
+//
+//         series: [{
+//             name: "Production",
+//             data: [16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
+//         }]
+//       })
