b1327337e742b809367feec77979a49996323534
Rori Kotch <r.kotch1@umiami.edu>
2017-04-26 11:59:29 -0400

    Added a second chart

--- script.js
+++ script.js
@@ -33,3 +33,38 @@ Highcharts.chart("container", {
         data: [492980000, 501954000, 520389000, 517261000, 538029000, 561722000, 589951000, 598166000, 617245000, 636971000, 693003000, 693797000, 713939000, 709599000, 738086000, 754667000, 782370000, 828272000, 819441000, 823902000, 842519000, 883505000, 911773000, 931691000, 962429000, 994542000, 1003278000, 1044983000]
     }]
   })
+
+  Highcharts.chart("containertwo", {
+
+      title: {
+          text: "U.S. Exports Exports by Volume in Metric Tons"
+      },
+
+      subtitle: {
+          text: "This data was sourced from the U.S.D.A. Department of Agriculture"
+      },
+
+      yAxis: {
+          title: {
+              text: "Millions of Pounds"
+          }
+      },
+
+      xAxis: {
+        categories: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
+          title: {
+              text: "Years"
+          }
+      },
+
+      plotOptions: {
+          series: {
+              pointStart: 2010
+          }
+      },
+
+      series: [{
+          name: "Production",
+          data: [28397, 74913, 78475, 66905, 75005, 68464, 82246]
+      }]
+    })
