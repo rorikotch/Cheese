d91d96b906c08522b61d9fa4cdc8d69632d52ee6
Rori Kotch <r.kotch1@umiami.edu>
2017-04-27 11:44:35 -0400

    Added in another script

--- script2.js
+++ script2.js
@@ -1,37 +1,3 @@
-// Highcharts.chart("container", {
-//     title: {
-//         text: "U.S. Milk Production 2010 to 2017"
-//     },
-//
-//     subtitle: {
-//         text: "This data was sourced from the U.S.D.A. Department of Agriculture"
-//     },
-//
-//     yAxis: {
-//         title: {
-//             text: "Millions of Pounds"
-//         }
-//     },
-//
-//     xAxis: {
-//       categories: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017],
-//         title: {
-//             text: "Years"
-//         }
-//     },
-//
-//     plotOptions: {
-//         series: {
-//             pointStart: 2010
-//         }
-//     },
-//
-//     series: [{
-//         name: "Production",
-//         data: [16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000]
-//     }]
-//   })
-
 Highcharts.chart("containerfour", {
 
     title: {
@@ -66,3 +32,64 @@ Highcharts.chart("containerfour", {
         data: [16013000000, 16396000000, 17048000000, 17114000000, 17278000000, 17690000000, 17693000000, 18145000000 ]
     }]
   });
+
+  $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=us-population-density.json&callback=?', function (data) {
+
+    // Make codes uppercase to match the map data
+    $.each(data, function () {
+        this.code = this.code.toUpperCase();
+    });
+
+  Highcharts.mapChart('containerfive', {
+
+          chart: {
+              borderWidth: 1
+          },
+
+          title: {
+              text: 'US Milk Producing States by Population of Dairy Cows'
+          },
+
+          legend: {
+              layout: 'horizontal',
+              borderWidth: 0,
+              backgroundColor: 'rgba(255,255,255,0.85)',
+              floating: true,
+              verticalAlign: 'top',
+              y: 25
+          },
+
+          mapNavigation: {
+              enabled: true
+          },
+
+          colorAxis: {
+              min: 1,
+              type: 'logarithmic',
+              minColor: '#EEEEFF',
+              maxColor: '#000022',
+              stops: [
+                  [0, '#EFEFFF'],
+                  [0.67, '#4444FF'],
+                  [1, '#000022']
+              ]
+          },
+
+          series: [{
+              animation: {
+                  duration: 1000
+              },
+              data: data,
+              mapData: Highcharts.maps['countries/us/us-all'],
+              joinBy: ['postal-code', 'code'],
+              dataLabels: {
+                  enabled: true,
+                  color: '#FFFFFF',
+                  format: '{point.code}'
+              },
+              name: 'dairy cow density',
+              tooltip: {
+                  pointFormat: '{point.code}: {point.value}/km²'
+              }
+          }]
+      });
